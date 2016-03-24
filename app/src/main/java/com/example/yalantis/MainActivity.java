56a2c60225cdc7e87886f55a86e761a6971f271b
Виталий Зеленский < >
2016-03-24 15:58:38 +0200

    Initial commit

---
+++ app/src/main/java/com/example/yalantis/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.yalantis;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private RVAdapter mRVAdapter;
+    private String[] mImagesUrl = new String[]{
+            "http://cs631918.vk.me/v631918154/209f5/BYy_UV61w1Y.jpg",
+            "http://cs631918.vk.me/v631918154/209fd/W4aSGtYMzY0.jpg",
+            "http://cs631918.vk.me/v631918154/20a05/IPWdyY46MMg.jpg"
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setTitle(R.string.title);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mRecyclerView = (RecyclerView)findViewById(R.id.recyclerView);
+        mRVAdapter = new RVAdapter(this, mImagesUrl);
+        LinearLayoutManager linearLayoutManager =
+                new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        mRecyclerView.setLayoutManager(linearLayoutManager);
+        mRecyclerView.setAdapter(mRVAdapter);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected (MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            Toast.makeText(MainActivity.this, getResources().getResourceEntryName(item.getItemId()),
+                    Toast.LENGTH_SHORT).show();
+            onBackPressed();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void toastShow(View view) {
+        Toast.makeText(MainActivity.this, getResources().getResourceEntryName(view.getId()),
+                Toast.LENGTH_SHORT).show();
+    }
+}
+
---
+++ app/src/main/java/com/example/yalantis/RVAdapter.java
@@ -0,0 +1,48 @@
+package com.example.yalantis;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+public class RVAdapter extends RecyclerView.Adapter<RVAdapter.MyViewHolder> {
+
+    private String[] mImagesUrl;
+    private Context mContext;
+    private ImageView mImage;
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+
+        public MyViewHolder (View view) {
+            super(view);
+
+            mImage = (ImageView) view.findViewById(R.id.myImage);
+        }
+    }
+
+    public RVAdapter (Context context, String[] imagesUrl) {
+        this.mContext = context;
+        this.mImagesUrl = imagesUrl;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder (ViewGroup parent, int viewType) {
+        View itemView =
+                LayoutInflater.from(parent.getContext()).inflate(R.layout.images, parent, false);
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder (MyViewHolder holder, int position) {
+        Picasso.with(mContext).load(mImagesUrl[position]).into(mImage);
+    }
+
+    @Override
+    public int getItemCount(){
+        return mImagesUrl.length;
+    }
+}
